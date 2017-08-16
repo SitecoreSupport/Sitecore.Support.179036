922086544ef39a0da2361ea85a1145bbfe1ac9ab
DK\psk <psk@sitecore.net>
2017-08-16 19:38:10 -0400

    Fix for "UnlockContactListsAgent stops working after stopping and resuming indexing"

---
+++ src/Sitecore.Support.179036/ListManagement/Analytics/UnlockContactListsAgent.cs
@@ -0,0 +1,27 @@
+﻿namespace Sitecore.Support.ListManagement.Analytics
+{
+    using Sitecore.ContentSearch;
+    using Sitecore.Abstractions;
+    using Sitecore.Analytics.Data.Bulk.Contact;
+    using Sitecore.Analytics.Model.Entities;
+    using Sitecore.ListManagement.ContentSearch.Model;
+    public class UnlockContactListsAgent : Sitecore.ListManagement.Analytics.UnlockContactListsAgent
+    {
+        public UnlockContactListsAgent(Sitecore.ListManagement.ListManager<ContactList, ContactData> listManager, Sitecore.ListManagement.ContentSearch.BatchIdMapper mapper, Sitecore.Analytics.Data.Bulk.BulkOperationManager<IContactTemplate, KnownContactSet, IContactUpdateResult> operationManager) : base(listManager, mapper, operationManager)
+        {
+        }
+
+        public UnlockContactListsAgent(Sitecore.ListManagement.ListManager<ContactList, ContactData> listManager, Sitecore.ListManagement.ContentSearch.BatchIdMapper mapper, Sitecore.Analytics.Data.Bulk.BulkOperationManager<IContactTemplate, KnownContactSet, IContactUpdateResult> operationManager, BaseLog logger) : base(listManager, mapper, operationManager, logger)
+        {
+        }
+            
+        public override void Execute()
+        {
+            if (Disabled)
+            {
+                Disabled = ContentSearchManager.GetAnalyticsIndex().IndexingState != IndexingState.Started;
+            }
+            base.Execute();
+        }
+    }
+}
---
+++ src/Sitecore.Support.179036/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.179036")]
+[assembly: AssemblyProduct("Sitecore.Support.179036")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("UnlockContactListsAgent stops working after stopping and resuming indexing")]
+
