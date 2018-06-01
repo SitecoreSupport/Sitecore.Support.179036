2d342fd2bfbe9f56ded30ec56619981f6cbccf87
DK\psk <psk@sitecore.net>
2018-06-01 09:45:50 +0300

    Adapt patch for 8.1.3

--- src/Sitecore.Support.179036/ListManagement/Analytics/UnlockContactListsAgent.cs
+++ src/Sitecore.Support.179036/ListManagement/Analytics/UnlockContactListsAgent.cs
@@ -1,7 +1,6 @@
 ﻿namespace Sitecore.Support.ListManagement.Analytics
 {
     using Sitecore.ContentSearch;
-    using Sitecore.Abstractions;
     using Sitecore.Analytics.Data.Bulk.Contact;
     using Sitecore.Analytics.Model.Entities;
     using Sitecore.ListManagement.ContentSearch.Model;
@@ -10,11 +9,6 @@
         public UnlockContactListsAgent(Sitecore.ListManagement.ListManager<ContactList, ContactData> listManager, Sitecore.ListManagement.ContentSearch.BatchIdMapper mapper, Sitecore.Analytics.Data.Bulk.BulkOperationManager<IContactTemplate, KnownContactSet, IContactUpdateResult> operationManager) : base(listManager, mapper, operationManager)
         {
         }
-
-        public UnlockContactListsAgent(Sitecore.ListManagement.ListManager<ContactList, ContactData> listManager, Sitecore.ListManagement.ContentSearch.BatchIdMapper mapper, Sitecore.Analytics.Data.Bulk.BulkOperationManager<IContactTemplate, KnownContactSet, IContactUpdateResult> operationManager, BaseLog logger) : base(listManager, mapper, operationManager, logger)
-        {
-        }
-            
         public override void Execute()
         {
             if (Disabled)
